0890aaaf0b0b5b72bb1b95ae67e5701a11b243f9
PS <palshankar@outlook.com>
2019-03-25 09:33:39 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(arr, value) {
+  return([value, ...arr]);
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, 'foo') {
+  arr.unshift(value);
+}
+
+function addElementToEndOfArray(arr, value) {
+  return([...arr, value]);
+}
+
+function destructivelyAddElementToEndOfArray(arr, value) {
+ arr.push(value);
+ return(arr);
+}
+
+function accessElementInArray(arr, index) {
+  return(arr[index]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  return(arr.shift());
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  return(arr.slice(1));
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return(arr);
+}
+
+function removeElementFromEndOfArray(arr) {
+  return(arr.slice(0, arr.length - 1));
+}
